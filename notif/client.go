3adebc129a0d9aa6a6dfaf456b6d55132643138c
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2025-12-28 18:57:59 -0500

    feat(notif) add call and email notifications

--- notif/client.go
+++ notif/client.go
@@ -2,6 +2,8 @@ package notif
 
 import (
 	"fmt"
+	"log/slog"
+	"net/smtp"
 
 	"github.com/twilio/twilio-go"
 	api "github.com/twilio/twilio-go/rest/api/v2010"
@@ -10,19 +12,25 @@ import (
 type Client struct {
 	twilioClient *twilio.RestClient
 	originNumber string
+
+	smtpHost string
+	smtpPort string
+	auth     smtp.Auth
+	from     string
 }
 
-func NewClient(accountSid, authToken, originNumber string) *Client {
+func NewClient(accountSid, authToken, originNumber, smtpHost, smtpPort, smtpUser, smtpPassword, from string) *Client {
 	// Find your Account SID and Auth Token at twilio.com/console
 	// and set the environment variables. See http://twil.io/secure
 	twilioClient := twilio.NewRestClientWithParams(twilio.ClientParams{
 		Username: accountSid,
 		Password: authToken,
 	})
-	return &Client{twilioClient, originNumber}
+	auth := smtp.PlainAuth("", smtpUser, smtpPassword, smtpHost)
+	return &Client{twilioClient, originNumber, smtpHost, smtpPort, auth, from}
 }
 
-func (c *Client) SendNotification(destinationNumber, message string) error {
+func (c *Client) call(destinationNumber, message string) error {
 	params := &api.CreateCallParams{}
 	params.SetTo(destinationNumber)
 	params.SetFrom(c.originNumber)
@@ -40,5 +48,60 @@ func (c *Client) SendNotification(destinationNumber, message string) error {
 		}
 	}
 
+	return nil
+}
+
+func (c *Client) text(destinationNumber, message string) error {
+	params := &api.CreateMessageParams{}
+	params.SetTo(destinationNumber)
+	params.SetFrom(c.originNumber)
+	params.SetBody(message)
+
+	resp, err := c.twilioClient.Api.CreateMessage(params)
+	if err != nil {
+		return fmt.Errorf("failed to send message: %w", err)
+	} else {
+		if resp.Sid != nil {
+			fmt.Println(*resp.Sid)
+		} else {
+			fmt.Println(resp.Sid)
+		}
+	}
+
+	return nil
+}
+
+func (c *Client) email(destinationEmail, message string) error {
+	// Manually construct the email message with headers
+	msg := []byte(fmt.Sprintf("To: %s\r\n"+
+		"Subject: %s\r\n"+
+		"\r\n"+
+		"%s\r\n", destinationEmail, "MVC Notification", message))
+
+	// Connect to the server and send the email
+	slog.Info("Sending email", "host", c.smtpHost, "port", c.smtpPort, "from", c.from, "to", destinationEmail, "msg", string(msg))
+	err := smtp.SendMail(c.smtpHost+":"+c.smtpPort, c.auth, c.from, []string{destinationEmail}, msg)
+	if err != nil {
+		return fmt.Errorf("failed to send email: %w", err)
+	}
+
+	return nil
+}
+
+func (c *Client) SendNotification(destinationNumber, destinationEmail, message string) error {
+	if destinationNumber != "" {
+		slog.Info("Calling", "number", destinationNumber)
+		err := c.call(destinationNumber, message)
+		if err != nil {
+			return fmt.Errorf("failed to send notification: %w", err)
+		}
+	}
+	if destinationEmail != "" {
+		slog.Info("Sending email", "email", destinationEmail)
+		err := c.email(destinationEmail, message)
+		if err != nil {
+			return fmt.Errorf("failed to send notification: %w", err)
+		}
+	}
 	return nil
 }
